d072238cc1e79c6fc484d14536b6614a6eb5ddee
Jean Piere <jeanpierefernandez80@gmail.com>
2024-08-24 00:18:43 -0500

    Modificacion: Contenido

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 window.addEventListener('load',()=>{
-    
+    let itemInicio = document.getElementById('itemInicio');
+    let itemAbout = document.getElementById('itemAbout');
+    let itemProyectos = document.getElementById('itemProyectos');
+    let itemContacto = document.getElementById('itemContacto');
+    let contenido_inicio = document.getElementById('contenido_inicio');
+    let contenido_about = document.getElementById('contenido_about');
+
+    itemAbout.addEventListener('click',()=>{
+        // contenido_inicio.style.backgroundColor = 'rgb(173, 175, 62)'; 
+    });
+
 });
