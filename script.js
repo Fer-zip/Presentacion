20d1fa0096bc094e5284eca73768c70667995518
Jean Piere <jeanpierefernandez80@gmail.com>
2024-08-26 00:45:35 -0500

    Modificando: sobre mi

--- script.js
+++ script.js
@@ -5,9 +5,35 @@ window.addEventListener('load',()=>{
     let itemContacto = document.getElementById('itemContacto');
     let contenido_inicio = document.getElementById('contenido_inicio');
     let contenido_about = document.getElementById('contenido_about');
+    let contenido_proyecto = document.getElementById('contenido_proyecto');
+    let contenido_contacto = document.getElementById('contenido_contacto');
+    let section = document.querySelectorAll('section');
+    let li = document.querySelectorAll('li');
 
-    itemAbout.addEventListener('click',()=>{
-        // contenido_inicio.style.backgroundColor = 'rgb(173, 175, 62)'; 
+    function ocultarSection() {
+        section.forEach(element =>{
+            element.style.display = 'none';
+        });
+    }
+
+    function mostrarSection(item){
+        ocultarSection();
+        item.style.display = 'flex'
+    }
+
+    itemInicio.addEventListener('click',()=>{
+        mostrarSection(contenido_inicio);
     });
 
+    itemAbout.addEventListener('click',()=>{
+        mostrarSection(contenido_about);
+    });
+
+    itemProyectos.addEventListener('click',()=>{
+        mostrarSection(contenido_proyecto);
+    });
+
+    itemContacto.addEventListener('click',()=>{
+        mostrarSection(contenido_contacto);
+    });
 });
