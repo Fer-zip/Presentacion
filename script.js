0b86e8a2891e090b72548efee4be596ae268b885
Jean Piere <jeanpierefernandez80@gmail.com>
2024-08-30 02:47:57 -0500

    Actualizacion: pagina 1 y 2

--- script.js
+++ script.js
@@ -8,7 +8,39 @@ window.addEventListener('load',()=>{
     let contenido_proyecto = document.getElementById('contenido_proyecto');
     let contenido_contacto = document.getElementById('contenido_contacto');
     let section = document.querySelectorAll('section');
-    let li = document.querySelectorAll('li');
+    // let li = document.querySelectorAll('li');
+    let letras = document.querySelectorAll('.animacion #letra');
+    let barra = document.getElementById('barra')
+    let index = 0;
+    let showing = true;
+    
+    let tiempo = setInterval(()=>{
+        if (barra.style.display=='none') {
+            barra.style.display='flex';
+        }else{
+            barra.style.display='none';
+        }
+    },300);
+
+    let time = setInterval(()=>{
+        if (showing) {
+            if (index < letras.length) {
+                letras[index].style.display = 'inline'; // Mostrar la siguiente letra
+                index++;
+            } else {
+                showing = false; // Cambiar a modo ocultar
+                index = letras.length - 1; // Empezar desde la última letra para ocultar
+            }
+        } else {
+            if (index >= 0) {
+                letras[index].style.display = 'none'; // Ocultar la letra actual
+                index--;
+            } else {
+                showing = true; // Cambiar a modo mostrar
+                index = 0; // Empezar desde la primera letra para mostrar
+            }
+        }
+    }, 400);
 
     function ocultarSection() {
         section.forEach(element =>{
